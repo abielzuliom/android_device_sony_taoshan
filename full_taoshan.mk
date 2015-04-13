b9640c1d5528cae6cc7d514e5de5f7bf3e607c0a
Abiel Zulio M <abielzm@gmail.com>
2015-04-13 09:30:48 +0700

    taoshan: slimroms bringup

--- full_taoshan.mk
+++ full_taoshan.mk
@@ -1,5 +1,4 @@
-#
-# Copyright 2013 The Android Open Source Project
+# Copyright (C) 2014 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,7 +11,19 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/full_taoshan.mk
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit taoshan device
+$(call inherit-product, device/sony/taoshan/taoshan.mk)
+
+# Inherit common Sony resources
+$(call inherit-product, device/sony/common/resources.mk)
+
+# Device identifier
+PRODUCT_DEVICE := taoshan
+PRODUCT_NAME := full_taoshan
+PRODUCT_BRAND := Sony
+PRODUCT_MODEL := Xperia L
+PRODUCT_MANUFACTURER := Sony
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -14,5 +14,7 @@
 # limitations under the License.
 #
 
-add_lunch_combo cm_taoshan-eng
-add_lunch_combo cm_taoshan-userdebug
+for i in eng userdebug user; do
+    add_lunch_combo full_taoshan-${i}
+    add_lunch_combo slim_taoshan-${i}
+done
